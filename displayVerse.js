7ac4376256b68b340132e1242b5bb5ca1ded07b7
Adam Wasilewski <chaper122@gmail.com>
2020-02-06 13:16:57 +0100

    first commit

---
+++ displayVerse.js
@@ -0,0 +1,44 @@
+var getJSON = function (url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+};
+
+
+function displayVerse(elementID, bible, book, chapter, verses=null, numbers=false) {
+    var element = document.getElementById(elementID);
+
+    let url = 'https://biblia.info.pl/api/biblia';
+    url += '/' + bible;
+    url += '/' + book;
+    url += '/' + chapter;
+    if (verses) {
+        url += '/' + verses;
+    }
+
+    getJSON(url, function(err, response) {
+        if (err != null) {
+            console.log(err);
+        } else {
+            response.wersety.forEach(verse => {
+                if (numbers) {
+                    let numberTag = document.createElement('number');
+                    numberTag.innerHTML = '(' + verse.werset + ')';
+                    element.append(numberTag);
+                }
+                let verseTag = document.createElement('verse');
+                verseTag.innerHTML = ' ' + verse.text + ' ';
+                element.append(verseTag);
+            })
+        }
+    })
+}
