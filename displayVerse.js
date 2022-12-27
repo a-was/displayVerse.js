97d8bbc803d649f60fd945c47dfc81a4f6809b44
a-was <adam.wasilewski@unix.com.pl>
2022-12-27 14:32:08 +0100

    Early return

--- displayVerse.js
+++ displayVerse.js
@@ -9,30 +9,29 @@ function displayVerse(elementId, bible, book, chapter, verses = null, numbers = 
         url += "/" + verses;
     }
 
-    getJSON(url, function (error, response) {
+    getJSON(url, function(error, response) {
         if (error !== null) {
             console.error(error);
             console.error(response);
-        } else {
-            const versesLength = response.verses.length
-            for (let i = 0; i < versesLength; i++) {
-                const verse = response.verses[i];
+            return;
+        }
+        const versesLength = response.verses.length
+        for (let i = 0; i < versesLength; i++) {
+            const verse = response.verses[i];
 
-                if (numbers) {
-                    const numberTag = document.createElement('span');
-                    numberTag.classList.add('verse', 'verse-number');
-                    numberTag.innerHTML = '(' + verse.verse + ')';
-                    element.append(numberTag);
-                }
-                const verseTag = document.createElement('span');
-                verseTag.classList.add('verse', 'verse-text');
-                verseTag.innerHTML = (i === 0 && numbers === false ? '' : ' ') + verse.text + (i === versesLength - 1 ? '' : ' ');
-                element.append(verseTag);
+            if (numbers) {
+                const numberTag = document.createElement("span");
+                numberTag.classList.add("verse", "verse-number");
+                numberTag.innerHTML = "(" + verse.verse + ")";
+                element.append(numberTag);
             }
+            const verseTag = document.createElement("span");
+            verseTag.classList.add("verse", "verse-text");
+            verseTag.innerHTML = (i === 0 && !numbers ? "" : " ") + verse.text + (i === versesLength - 1 ? "" : " ");
+            element.append(verseTag);
         }
     })
 }
-
 
 function getJSON(url, callback) {
     const xhr = new XMLHttpRequest();
